b4fcf43ca87c0bf72474ca21c69ec623d1189cd2
Alexperovo <alexperovo@gmail.com>
2022-11-22 21:39:43 +0300

    работает

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите цикл, который принимает на вход 2 числа (А и Б) и возводит число А в натуральную степеть Б. 
+// 3,5 -> 243, 2,4 ->16
+Console.WriteLine("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int firstnum = a;
+ for (int i = 1; i < b; i++)
+   {
+    firstnum = firstnum * a;
+   }
+Console.WriteLine("Число А в натуральной степени числа Б равна: " + firstnum);
